1307c3adf1b729c3dafb79e95c9a0214e97cf297
Shelvin Batista Batista <shelvin.batista@quick.com.co>
2023-01-11 11:14:41 -0500

    Configuracion

--- ServiciosRest/settings.py
+++ ServiciosRest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-megf+jruy&ay@*-&p7a@a%w($jycjhx*uwq-qgm2qvhe-r5et7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
